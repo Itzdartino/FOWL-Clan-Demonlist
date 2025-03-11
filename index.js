d71cf6ef7ac6c53be109731fb1c3690b9d939532
ThatGuyNick <nickrudnick800@gmail.com>
2025-03-10 20:25:47 -0400

    public folder website files test 3.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
-
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    
+    // Default to serving index.html if root is requested
+    let path = url.pathname === "/" ? "/index.html" : url.pathname;
+
     try {
-      return await getAssetFromKV(request, { mapRequestToAsset: req => new Request(req.url, { redirect: 'follow' }) });
+      return await env.ASSETS.fetch(new Request(url.origin + "/public" + path, request));
     } catch (e) {
-      return new Response('404 Not Found', { status: 404 });
+      return new Response("404 Not Found", { status: 404 });
     }
   },
 };
