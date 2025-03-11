366c782ae72e09f98add910c0c06d2bbb1623db3
ThatGuyNick <nickrudnick800@gmail.com>
2025-03-10 20:17:49 -0400

    public folder website files test.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
 export default {
-  async fetch(request) {
-    return new Response("This is FOWL Clan Demonlist!", {
-      headers: { "Content-Type": "text/html" },
-    });
+  async fetch(request, env, ctx) {
+    try {
+      return await getAssetFromKV(request, { mapRequestToAsset: req => new Request(req.url, { redirect: 'follow' }) });
+    } catch (e) {
+      return new Response('404 Not Found', { status: 404 });
+    }
   },
 };
