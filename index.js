efb0f1214c8fe2c74ed5ad70772e2fa7cfef4537
ThatGuyNick <nickrudnick800@gmail.com>
2025-03-10 20:28:40 -0400

    Fixed file serving for Cloudflare

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 export default {
   async fetch(request, env) {
-    let url = new URL(request.url);
-    
-    // Default to serving index.html if root is requested
-    let path = url.pathname === "/" ? "/index.html" : url.pathname;
-
-    try {
-      return await env.ASSETS.fetch(new Request(url.origin + "/public" + path, request));
-    } catch (e) {
-      return new Response("404 Not Found", { status: 404 });
-    }
+    return env.ASSETS.fetch(request);
   },
 };
